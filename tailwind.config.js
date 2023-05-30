87c97f1b22dc3f2763bdb605fa7832b31d681346
Staniuu <staniuu123@gmail.com>
2023-05-30 10:41:05 +0200

    developing: project's scaffolding

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./app/**/*.{ts,tsx}',
+		'./context/**/*.{ts,tsx',
+		'./components/**/*.{ts,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				darkText: 'hsl(0, 0%, 100%)',
+				darkElement: 'hsl(209, 23%, 22%)',
+				darkBackground: 'hsl(207, 26%, 17%)',
+				lightText: 'hsl(200, 15%, 8%)',
+				lightInput: 'hsl(0, 0%, 52%)',
+				lightElement: 'hsl(0, 0%, 100%)',
+				lightBackground: 'hsl(0, 0%, 98%)',
+			},
+		},
+		fontSize: {
+			small: '14px',
+			big: '16px',
+		},
+		fontFamily: {
+			sans: ['Nunito Sans', 'sans-serif'],
+		},
+		fontWeight: {
+			light: 300,
+			medium: 600,
+			heavy: 800,
+		},
+		screens: {
+			md: '620px',
+		},
+	},
+	plugins: [],
+}
