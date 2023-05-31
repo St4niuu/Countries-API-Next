ba370332d2ba8228179cc0bd8dd0bcbb094e7f30
Staniuu <staniuu123@gmail.com>
2023-05-31 14:37:22 +0200

    developing: input's component's correction

--- components/Input.tsx
+++ components/Input.tsx
@@ -3,7 +3,7 @@
 import { useAppContext } from '@/context/AppContext'
 
 export default function Input(): JSX.Element {
-	const { theme, inputValue, setInputValue } = useAppContext()
+	const { inputValue, setInputValue } = useAppContext()
 
 	return (
 		<div className='w-full min-w-[300px] h-16 bg-white flex items-center gap-6 pl-10 pr-6 rounded-md shadow-xl md:w-1/6 dark:bg-darkElement'>
@@ -13,6 +13,8 @@ export default function Input(): JSX.Element {
 			<input
 				className='bg-transparent grow caret-gray-500 dark:text-white dark:caret-white dark:placeholder-white'
 				placeholder='Search for a country... '
+				value={inputValue}
+				onChange={(e) => setInputValue(e.target.value)}
 			/>
 		</div>
 	)
